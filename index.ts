10d755b70971324270c9df191dbb3b32d368cb7d
Kwek Ming Hong <minghong.k@affinidi.com>
2023-08-04 15:42:47 +0800

    feat: add ping feature

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import { exec } from 'child_process';
 
 const app: Express = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, world!')
+})
+
+app.get('/ping/:ip', (req: Request, res: Response) => {
+    exec('ping -c 4 ' + req.params['ip'], (err, stdout, stderr) => {
+        if (err) {
+            res.send(stderr);
+        } else {
+            res.send(stdout);
+        }
+    })
 })
 
 app.listen(port, () => {
